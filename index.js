67c24ad301486dacbf973119023930a319d270f8
Tom Lynch <twhlynch@gmail.com>
2026-07-21 21:57:15 +1000

    correctly parse spaces

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ inputElement.addEventListener('change', () => {
 });
 
 function convert(arcData) {
-	let items = parseArcData(arcData);
+	let items = parseBookmarks(arcData);
+	let spaces = parseSpaces(arcData);
 
 	let bookmarks = [];
 
@@ -36,18 +37,114 @@ function convert(arcData) {
 		}
 	}
 
-	// remove unpinned tabs
-	bookmarks = bookmarks.filter(
-		(bm) =>
-			bm.ID !==
-			'thebrowser.company.defaultPersonalSpaceUnpinnedContainerID',
-	);
+	let allUnpinnedIDs = new Set();
+	for (let space of spaces) {
+		for (let id of space.unpinnedContainerIDs) {
+			allUnpinnedIDs.add(id);
+		}
+	}
 
-	console.log(bookmarks);
-	return convertJsonToBookmarks(bookmarks);
+	let topAppsItem = bookmarks.find((b) => b.isTopApps);
+
+	let result = [];
+
+	for (let space of spaces) {
+		let pinnedItem = bookmarks.find(
+			(b) => b.ID === space.pinnedContainerID,
+		);
+		if (!pinnedItem) continue;
+
+		let spaceBookmarks = [];
+
+		if (
+			topAppsItem &&
+			space.id === 'thebrowser.company.defaultPersonalSpaceID'
+		) {
+			spaceBookmarks.push(...topAppsItem.children);
+		}
+
+		spaceBookmarks.push(
+			...pinnedItem.children.filter(
+				(child) => !allUnpinnedIDs.has(child.ID),
+			),
+		);
+
+		if (spaceBookmarks.length) {
+			result.push({ title: space.title, bookmarks: spaceBookmarks });
+		}
+	}
+
+	return convertJsonToBookmarks(result);
 }
 
-function convertJsonToBookmarks(items) {
+function parseSpaces(data) {
+	let spaces = [];
+	const containers = data.sidebar.containers;
+	for (const container of containers) {
+		if (container.spaces) {
+			for (let i = 0; i < container.spaces.length; i++) {
+				const entry = container.spaces[i];
+				if (typeof entry === 'string') {
+					const spaceObj = container.spaces[i + 1];
+					if (spaceObj && spaceObj.containerIDs) {
+						let pinnedContainerID = null;
+						let unpinnedContainerIDs = [];
+						const containerIDs = spaceObj.containerIDs;
+						for (let j = 0; j < containerIDs.length; j++) {
+							if (
+								containerIDs[j] === 'pinned' &&
+								j + 1 < containerIDs.length
+							) {
+								pinnedContainerID = containerIDs[j + 1];
+							} else if (
+								containerIDs[j] === 'unpinned' &&
+								j + 1 < containerIDs.length
+							) {
+								unpinnedContainerIDs.push(containerIDs[j + 1]);
+							}
+						}
+						spaces.push({
+							id: entry,
+							title: spaceObj.title,
+							pinnedContainerID,
+							unpinnedContainerIDs,
+						});
+					}
+				}
+			}
+		}
+	}
+	return spaces;
+}
+
+function parseBookmarks(data) {
+	let items = [];
+	const containers = data.sidebar.containers;
+	for (const container of containers) {
+		if (container.items) {
+			for (const item of container.items) {
+				if (typeof item !== 'string') {
+					items.push({
+						ID: item.id || null,
+						url: item.data?.tab?.savedURL || null,
+						title:
+							item.title ||
+							item.data?.tab?.savedTitle ||
+							'Pinned',
+						parentID: item.parentID || null,
+						childIDs: item.childrenIds || [],
+						children: [],
+						isTopApps:
+							!!item.data?.itemContainer?.containerType?.topApps,
+					});
+				}
+			}
+		}
+	}
+	return items;
+}
+
+function convertJsonToBookmarks(spaces) {
 	let bookmarks = `
 <!DOCTYPE NETSCAPE-Bookmark-file-1>
 <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
@@ -56,12 +153,21 @@ function convertJsonToBookmarks(items) {
 <DL><p>
   <DT><H3 PERSONAL_TOOLBAR_FOLDER="true">Bookmarks Bar</H3>
   <DL><p>
-    ${items.map((item) => createBookmark(item)).join('\n')}
+    ${spaces.map((space) => createSpace(space)).join('\n')}
   </DL><p>
 </DL><p>
 `;
 
 	return bookmarks;
+}
+
+function createSpace(space) {
+	return `
+<DT><H3>${space.title}</H3>
+<DL><p>
+  ${space.bookmarks.map((item) => createBookmark(item)).join('\n')}
+</DL><p>
+`;
 }
 
 function createBookmark(item) {
@@ -75,32 +181,6 @@ function createBookmark(item) {
 		: `
 <DT><A HREF="${item.url}" >${item.title}</A>
 `;
-}
-
-function parseArcData(data) {
-	let items = [];
-	const containers = data.sidebar.containers;
-	for (const container of containers) {
-		if (container.items) {
-			for (const item of container.items) {
-				if (typeof item !== 'string') {
-					console.log(item);
-					items.push({
-						ID: item.id || null,
-						url: item.data?.tab?.savedURL || null,
-						title:
-							item.title ||
-							item.data?.tab?.savedTitle ||
-							'Pinned',
-						parentID: item.parentID || null,
-						childIDs: item.childrenIds || [],
-						children: [],
-					});
-				}
-			}
-		}
-	}
-	return items;
 }
 
 function download(text) {
